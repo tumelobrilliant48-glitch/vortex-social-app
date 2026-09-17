7fb13cca756270f47dd7bc6f50eafe26b28a5f78
tumelobrilliant48-glitch <tumelobrilliant48@gmail.com>
2026-09-17 20:54:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,6 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('install', e=>{
+ e.waitUntil(caches.open('vortex-v2').then(c=>c.addAll(['./','./index.html','./manifest.json'])))
+});
+self.addEventListener('fetch', e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+});
